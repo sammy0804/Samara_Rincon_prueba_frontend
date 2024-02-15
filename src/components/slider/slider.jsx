046b8f9559a5f3b75519e49f3100a51feb23adf9
sammy0804 <51298297+sammy0804@users.noreply.github.com>
2024-02-15 14:46:17 -0500

    Fixed little mistakes with react and node versions

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import Slider from 'react-slick';
 import Card from './Card';
 
 //styles slide
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
-import "../../styles/slider.css"
+import "slick-carousel/slick/slick-theme.css";
+import "../../styles/slider.css";
 
 const CardSlider = ({ cards }) => {
  
