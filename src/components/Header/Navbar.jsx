994b44e3b2b3da53592774cc6f9a8f5b62bf64d0
sammy0804 <51298297+sammy0804@users.noreply.github.com>
2024-02-14 19:57:07 -0500

    added responsive in the navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import {menuItems} from "../../data/MenuItems";
 import MenuItems from "./MenuItems";
 
 const Navbar = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
-        <nav>
-            <ul className="menus">
+        <nav className='navbar'>
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                {/* Icon for the hamburger menu */}
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <ul className={`menus ${isOpen ? "show" : ""}`}>
                 {
                     menuItems.map((menu, index)=>{
                         const depthlevel = 0;
